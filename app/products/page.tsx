08ba4a0910824eddb117150fbe49e684e6872009
Victor Nascimento <victorcstsilva.ads@outlook.com>
2023-07-13 10:46:11 -0300

    estrutura do projeto next

---
+++ app/products/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Products = () => {
+  return (
+    <>
+    <Head>
+      <title>teste</title>
+    </Head>
+      <div>Products</div>
+    </>  
+
+  )
+}
+
+export default Products
